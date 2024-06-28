131ecdd5d7a0834839df991d0d6661c335fde071
PLeal7 <pleal1040@gmail.com>
2024-06-28 00:31:06 -0300

    Added main files

---
+++ index.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createMainWindow(){
+	const mainWindow = new BrowserWindow({
+		title: 'Image Resizer',
+		width: 500,
+		height: 600,
+	});
+
+	mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+}
+
+app.whenReady().then(() => {
+	createMainWindow();
+});
+
