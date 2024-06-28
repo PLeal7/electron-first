e880fd732c7732ea1a066b15f030676b6085d362
PLeal7 <pleal1040@gmail.com>
2024-06-28 12:03:01 -0300

    Added assets and main boilerplate

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
+
+const isDev = process.env.NODE_ENV !== 'development';
+const isMac = process.platform === 'darwin';
 
 function createMainWindow(){
 	const mainWindow = new BrowserWindow({
 		title: 'Image Resizer',
-		width: 500,
+		width: isDev ? 1000 : 500,
 		height: 600,
 	});
+
+	if(isDev){
+		mainWindow.webContents.openDevTools();
+	}
 
 	mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
 app.whenReady().then(() => {
 	createMainWindow();
+
+	const mainMenu = Menu.buildFromTemplate(menu);
+	Menu.setApplicationMenu(mainMenu);
 });
 
+app.on('activate', () => {
+	if (BrowserWindow.getALLWindows().length === 0){
+		createMainWindow();
+	}
+})
+
+const menu = [
+	{
+		label: "File",
+		submenu: [
+			{
+				label: 'Quit'
+				click: () => app.quit(),
+				accelerator: 'CmdOrCtrl+W'
+			}
+		]
+	}
+]
+
+app.on('window-all-closed', () => {
+	if (!isMac){
+		app.quit();
+	}
+})
